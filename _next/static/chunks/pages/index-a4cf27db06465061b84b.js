98465930196f13a038ec19e1e7860f3534f94ee7
vitaly-bogdanov <kaliummati@gmail.com>
2021-05-30 05:35:32 +0000

    Deploying to gh-pages from  @ 5786b1287d16225d397dbdb176f59187268314f6 🚀

---
+++ _next/static/chunks/pages/index-a4cf27db06465061b84b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5722:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return r}});var t=_(8923),r=!0;u.default=t.G0},5301:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(5722)}])}},function(n){n.O(0,[774,888,179],(function(){return u=5301,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
---
+++ _next/static/chunks/pages/symbol-672cd5c0d2945398785a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[754],{47:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return r}});var t=_(6352),r=!0;u.default=t.vE},1069:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/symbol",function(){return _(47)}])}},function(n){n.O(0,[774,888,179],(function(){return u=1069,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
